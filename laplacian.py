5de88d7a1c45b7709da442ac2eb75c10ee19e787
Patrick Hennessy <patrickhennessy94@gmail.com>
2022-12-08 22:06:11 -0400

    Loop through a folder of images

--- laplacian.py
+++ laplacian.py
@@ -1,12 +1,22 @@
 import cv2
+import os
 
-img = cv2.imread("images\dog.jpg")
+path = "C:\\images\\examples\\"
+var_total = 0
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_list = os.listdir(path)
+#print(len(img_list))
 
-lap = cv2.Laplacian(gray, cv2.CV_64F)
-var = lap.var()
+for f in img_list:
+    img = cv2.imread(path + f)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-print(lap)
-print(len(lap))
-print(var)
+    lap = cv2.Laplacian(gray, cv2.CV_64F)
+    var = lap.var()
+    var_total += var
+
+    #print(lap)
+    #print(len(lap))
+    #print(var)
+
+print('Avg var: ', var_total/len(img_list))
